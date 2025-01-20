9478e19a0669dc5095c58f9d1b5f102568e41dce
Ducky <97487136+bruvzz@users.noreply.github.com>
2025-01-19 22:19:55 -0500

    Updated purge.js

--- src/commands/moderation/purge.js
+++ src/commands/moderation/purge.js
@@ -5,6 +5,10 @@ const {
     PermissionFlagsBits,
     EmbedBuilder,
 } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+
+const logChannelsPath = path.join(__dirname, "../logChannels.json");
 
 module.exports = {
     /**
@@ -52,6 +56,33 @@ module.exports = {
                 .setTimestamp();
 
             await interaction.editReply({ embeds: [embed] });
+
+            let logChannels = {};
+            if (fs.existsSync(logChannelsPath)) {
+                logChannels = JSON.parse(fs.readFileSync(logChannelsPath, "utf-8"));
+            }
+
+            const logChannelId = logChannels[interaction.guild.id];
+            if (logChannelId) {
+                const logChannel = interaction.guild.channels.cache.get(logChannelId);
+                if (logChannel) {
+                    const logEmbed = new EmbedBuilder()
+                        .setColor("#4ea554")
+                        .setTitle("Purge Log")
+                        .setDescription(
+                            `<@${interaction.user.id}> (**${interaction.user.id}**) purged **${deletedMessages.size} ${messageWord}** in <#${interaction.channel.id}>.`
+                        )
+                        .setFooter({
+                            text: `Action performed by ${interaction.user.tag}`,
+                            iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
+                        })
+                        .setTimestamp();
+
+                    await logChannel.send({ embeds: [logEmbed] });
+                } else {
+                    console.error("Log channel not found.");
+                }
+            }
         } catch (error) {
             console.error("Error deleting messages:", error);
             await interaction.editReply({
