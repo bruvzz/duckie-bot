17e9aaf2298d1bb06590ee0be787f42cd8e76d58
Ducky <97487136+bruvzz@users.noreply.github.com>
2025-07-20 23:17:56 -0400

    Updated faq.js

--- src/commands/misc/faq.js
+++ src/commands/misc/faq.js
@@ -17,14 +17,14 @@ module.exports = {
       const guild = interaction.guild;
 
       const embed = new EmbedBuilder()
-        .setColor("#4ea554")
+        .setColor("Grey")
         .setAuthor({
             name: "Official Submarine Bot",
             iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
         })
         .setTitle("Greetings,")
         .setDescription(
-            `<@${interaction.user.id}>\n\n__*Frequently Asked Questions*__:\n> All questions and/or inquiries can be handled and assisted by our <@&1335724055755493486> team. If one cannot assist you, please seek someone else or a higher position for help.\n\n__*Common Fixes*__:\n> Temporarily, you can find any fixes listed [here](https://docs.google.com/document/d/1200GhxUFHryDlU-WB4SK1FAqx06PPxgTzz-iDrPU8q8/edit?tab=t.0#heading=h.1fob9te). In the meantime, we are currently working on getting our support system back online and restore its functionality.`
+            `<@${interaction.user.id}>\n\n__*Frequently Asked Questions*__:\n> All questions and/or inquiries can be handled and assisted by our <@&1361711126609661969> team. If one cannot assist you, please seek someone else or a higher position for help.\n\n__*Common Fixes*__:\n> Temporarily, you can find any fixes listed [here](https://docs.google.com/document/d/1200GhxUFHryDlU-WB4SK1FAqx06PPxgTzz-iDrPU8q8/edit?tab=t.0#heading=h.1fob9te). In the meantime, we are currently working on getting our support system back online and restore its functionality.`
         )
         .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true, size: 1024 }))
         .setFooter({
