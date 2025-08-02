7d07931ed0e6562a493edb7bcf237b74bf44f373
Ducky <97487136+bruvzz@users.noreply.github.com>
2025-08-02 16:39:24 -0400

    Updated lock.js

--- src/commands/moderation/lock.js
+++ src/commands/moderation/lock.js
@@ -50,7 +50,7 @@ module.exports = {
       });
 
       const confirmationEmbed = new EmbedBuilder()
-        .setColor("#4ea554")
+        .setColor("Grey")
         .setTitle("Channel Locked")
         .setDescription(`<#${targetChannel.id}> has been locked for <@&${targetRole.id}>.`)
         .addFields({ name: "Reason", value: reason })
